633107d396c5405cb134d7ef68e6f3c4894110cb
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-11-19 15:30:23 -0800

    added Game.js constructor and methods to be filled

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,33 @@
 class Game{
-  constructor(){
-    
+  constructor(missed, phrases){
+this.missed = missed;
+this.phrases = [];
   }
-}
+  getRandomPhrase(){
+    const phrases = ["test phrase",
+    "win condition",
+     "stuff"];
+     var random = Math.random() * phrases.length;
+     console.log(phrases[random]);
+     return phrases[random];
+
+
+  }//end method getRandomPhrase()
+  handleInteraction(){
+
+
+
+  }//end method handleInteraction()
+  removeLife(){//call when checkLetter returns false
+
+  }//end method removeLife()
+  checkForWin(){//call when all letters have been revealed
+
+  }//end method checkForWin()
+  gameOver(){//call when all hearts are lost
+
+  }//end method gameOver()
+  startGame(){//calls the getRandomPhrase() method, and adds that phrase to the board by calling the Phrase class' addPhraseToDisplay() method.
+  }//end method startGame()
+
+}//end class Game
