99152b32d2161bf95625da1d5cfd865a80ff8ce8
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-11-19 14:57:51 -0800

    reconfigured checkLetter and showMatchedLetter in Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,6 +1,6 @@
 class Phrase {
   constructor(phrase) {
-    this.phrase = phrase
+    this.phrase = phrase;
   }
   addPhraseToDisplay() {
     //this value is a String
@@ -18,28 +18,33 @@ class Phrase {
     } //end for-loop
   } //end method addPhraseToDisplay()
 
-  checkLetter() {
+  checkLetter(letter) {
     //Checks if a letter matches the button pressed?
     //
     let buttonKeys = [...document.querySelectorAll(".key")];
     const keyText = buttonKeys.map(key=> key.textContent.toUpperCase());
     const li =[...document.querySelectorAll("#phrase ul li")];
     const liText = li.map(list => list.textContent);
-    const newArray = buttonKeys.filter(button => liText.includes(button.textContent.toUpperCase()));
-    const falseArray = buttonKeys.filter(button => !liText.includes(button.textContent.toUpperCase()));
-    console.log(newArray);
-    newArray.forEach(button=> button.addEventListener('click', function (){
-      console.log("true");
-      return true;
-    }));
-    falseArray.forEach(button=> button.addEventListener('click', function (){
-      console.log("false");
-      return false;
-    }));
+    for (let i = 0; i < this.phrase.length; i++) {
+      if(this.phrase[i]===letter){
+        console.log("this is a correct choice");
+        return true
+      } else {
+        console.log("this is a false choice");
+      }
+    }
+
 
 
   } //end method checkLetter()
-  showMatchedLetter() {
-  console.log(this.checkLetter());
-  } //end method showMatchedLetter()
+  showMatchedLetter(letter){
+    const li =[...document.querySelectorAll("#phrase ul li")];
+    const liText = li.map(list => list.textContent);
+    for (let i = 0; i < this.phrase.length; i++) {
+      if(this.phrase[i]===letter){
+        li[i].className = "show letter";
+      }
+    }
+
+  }//end method showMatchedLetter()
 } //end Phrase()
--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ const phrase = new Phrase("test phrase".toUpperCase());
 console.log("app js test");
 document.querySelector("#overlay").style.display = "none";
 phrase.addPhraseToDisplay();//will need to make 'array' of phrases
-if( phrase.checkLetter() ){
-  console.log("this is working");
-}
-phrase.showMatchedLetter();
+const buttonKeys = [...document.querySelectorAll(".key")]
+buttonKeys.map(button=>button.addEventListener('click',function(e){
+ if(phrase.checkLetter(e.target.textContent.toUpperCase())){
+   phrase.showMatchedLetter(e.target.textContent.toUpperCase());
+  }
+}));
+
+// phrase.showMatchedLetter();
